7405bff7db9521adfabf2b356e4f874618783904
eugene <eugenefromrus@gmail.com>
2021-06-08 23:51:42 +1000

    промежуточный

--- js/app.min.js
+++ js/app.min.js
@@ -5,24 +5,42 @@ document.addEventListener("DOMContentLoaded", () => {
     const modals = document.querySelectorAll('[data-type="modal"]');
     const counters = document.querySelectorAll('[data-type="counter"]');
     const menu = document.getElementById("menu");
+    const items = document.querySelectorAll("#paymentAccordion .payment-delivery__item-title");
+    const topNavItems = document.querySelectorAll("#menu .menu__item-title");
     const menuClose = document.getElementById("menuClose");
     const topNav = document.getElementById("topNav");
     const menuBtn = document.getElementById("menuBtn");
     const search = document.getElementById("search");
     const callSearch = document.getElementById("callSearch");
     const searchReset = document.getElementById("searchReset");
+    const preloader = document.getElementById("preloader");
+    const preloaderBtn = document.getElementById("preloaderBtn");
+
+    if (preloader && preloaderBtn) {
+      preloaderBtn.addEventListener("click", function (e) {
+        preloader.classList.toggle("active");
+      });
+    }
+
+    if (topNavItems) {
+      topNavItems.forEach(item => {
+        item.addEventListener("click", function (e) {
+          item.classList.toggle("active");
+        });
+      });
+    }
 
     if (search && callSearch) {
-      callSearch.addEventListener('click', function(e) {
-        search.classList.toggle('active')
-        if(window.innerWidth <= 1100) {
-          document.body.classList.toggle('hiddenY')
+      callSearch.addEventListener("click", function (e) {
+        search.classList.toggle("active");
+        if (window.innerWidth <= 1100) {
+          document.documentElement.classList.toggle("hiddenY");
         }
-      }) 
-      searchReset.addEventListener('click', function(e) {
-        search.classList.remove('active')
-        document.body.classList.remove('hiddenY')
-      }) 
+      });
+      searchReset.addEventListener("click", function (e) {
+        search.classList.remove("active");
+        document.documentElement.classList.remove("hiddenY");
+      });
     }
 
     if (counters && counters.length > 0) {
@@ -58,13 +76,13 @@ document.addEventListener("DOMContentLoaded", () => {
         menu.classList.toggle("menu_active");
         topNav.classList.toggle("top-nav_active");
         search.classList.toggle("active-menu");
-        document.body.classList.toggle('hiddenY');
+        document.documentElement.classList.toggle("hiddenY");
       });
       menuClose.addEventListener("click", function () {
         menu.classList.remove("menu_active");
         topNav.classList.remove("top-nav_active");
         search.classList.remove("active-menu");
-        document.body.classList.remove('hiddenY');
+        document.documentElement.classList.remove("hiddenY");
       });
     }
 
@@ -75,6 +93,7 @@ document.addEventListener("DOMContentLoaded", () => {
           const modal = document.getElementById(name);
           if (modal) {
             modal.classList.add("active");
+            document.documentElement.classList.add("hiddenY");
           }
         });
       });
@@ -83,8 +102,9 @@ document.addEventListener("DOMContentLoaded", () => {
     if (modals) {
       modals.forEach(item => {
         item.addEventListener("click", function (e) {
-          if (e.target.classList.contains("modal__top-close-img") || e.target.classList.contains("modal__container") || e.target.classList.contains("modal") || e.target.classList.contains("subscribe-modal__close-img") || e.target.dataset?.type?.includes("modal")) {
+          if (e.target.classList.contains("modal__top-close-img") || e.target.classList.contains("modal__container") || e.target.classList.contains("modal") || e.target.classList.contains("table-size") || e.target.classList.contains("filter__header-close") || e.target.classList.contains("table-size__header-close") || e.target.classList.contains("subscribe-modal__close-img") || e.target.dataset?.type?.includes("modal")) {
             item.classList.remove("active");
+            document.documentElement.classList.remove("hiddenY");
           }
         });
       });
@@ -100,6 +120,18 @@ document.addEventListener("DOMContentLoaded", () => {
       navigation: {
         nextEl: ".promotions__arrows .promotions__arrow.promotions__arrow_next",
         prevEl: ".promotions__arrows .promotions__arrow.promotions__arrow_prev"
+      },
+      breakpoints: {
+        // when window width is >= 0
+        0: {
+          slidesPerView: 2,
+          spaceBetween: 10
+        },
+        // when window width is >= 320px
+        576: {
+          slidesPerView: 3,
+          spaceBetween: 30
+        }
       }
     });
   };
@@ -120,26 +152,32 @@ document.addEventListener("DOMContentLoaded", () => {
           nextEl: ".text-with-slider__arrows .text-with-slider__arrow.text-with-slider__arrow_next",
           prevEl: ".text-with-slider__arrows .text-with-slider__arrow.text-with-slider__arrow_prev"
         },
+        breakpoints: {
+          // when window width is >= 0
+          0: {
+            spaceBetween: 10
+          },
+          // when window width is >= 991px
+          991: {
+            spaceBetween: 30
+          }
+        },
         on: {
           init: function () {
             const count = this.slides.length;
             const active = this.activeIndex;
             this.slideTo(1);
             if (currentSlideEl && allSlidesEl) {
-              if (loop) {
-                currentSlideEl.innerHTML = active + 1;
-                allSlidesEl.innerHTML = count;
-              }
+              currentSlideEl.innerHTML = this.activeIndex;
+              allSlidesEl.innerHTML = `/ ${this.slides.length}`;
             }
           },
           slideChange: function () {
             const count = this.slides.length;
             const active = this.activeIndex;
             if (currentSlideEl && allSlidesEl) {
-              if (loop) {
-                currentSlideEl.innerHTML = active + 1;
-                allSlidesEl.innerHTML = count;
-              }
+              currentSlideEl.innerHTML = this.activeIndex + 1;
+              allSlidesEl.innerHTML = `/ ${this.slides.length}`;
             }
           }
         }
@@ -155,11 +193,8 @@ document.addEventListener("DOMContentLoaded", () => {
           prevEl: ".top-goods__arrows .top-goods__arrow.top-goods__arrow_prev"
         },
         on: {
-          init: function () {
-          },
-          slideChange: function () {
-            
-          }
+          init: function () {},
+          slideChange: function () {}
         }
       });
     })();
@@ -167,50 +202,68 @@ document.addEventListener("DOMContentLoaded", () => {
     (() => {
       const currentSlideEl = document.getElementById("currentSlideAbout");
       const allSlidesEl = document.getElementById("allSlidesAbout");
+
       let leftImgWrapper = document.getElementById("aboutSliderLeftImgWrapper");
       let leftImg = document.getElementById("aboutSliderLeftImg");
 
-      const loop = true;
-      var abloutSlider = new Swiper("#abloutSlider", {
-        slidesPerView: 1,
+      let rightImgWrapper = document.getElementById("aboutSliderRightImgWrapper");
+      let rightImg = document.getElementById("aboutSliderRightImg");
+
+      var aboutSlider = new Swiper("#aboutSlider", {
+        slidesPerView: 3,
         spaceBetween: 30,
-        loop: loop,
+        loop: false,
         navigation: {
           nextEl: ".about-slider__arrows .about-slider__arrow.about-slider__arrow_next",
           prevEl: ".about-slider__arrows .about-slider__arrow.about-slider__arrow_prev"
+        },
+        breakpoints: {
+          0: {
+            slidesPerView: 1,
+            spaceBetween: 0
+          },
+          576: {
+            slidesPerView: 2,
+            spaceBetween: 30
+          },
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 30
+          }
         },
         on: {
           init: function () {
             const count = this.slides.length;
             const active = this.activeIndex;
 
-            const currentSlide = document.querySelector("#abloutSlider .swiper-slide-active img");
+            const currentSlide = document.querySelector("#aboutSlider .swiper-slide-active img");
 
-            // currentSlideEl.innerHTML = this.activeIndex;
-            // allSlidesEl.innerHTML = `/ ${this.slides.length}`;
+            currentSlideEl.innerHTML = this.activeIndex + 1;
+            allSlidesEl.innerHTML = `/ ${this.slides.length}`;
           },
-          slideChange: function () {
+          transitionEnd: function () {
             // images
-            const currentSlide = document.querySelector("#abloutSlider .swiper-slide-active img");
+            const currentSlide = document.querySelector("#aboutSlider .swiper-slide-active img");
 
             leftImgWrapper.classList.add("changing");
+            rightImgWrapper.classList.add("changing");
             setTimeout(() => {
               leftImg.src = currentSlide.src;
+              rightImg.src = currentSlide.src;
             }, 350);
             setTimeout(() => {
               leftImgWrapper.classList.remove("changing");
+              rightImgWrapper.classList.remove("changing");
             }, 500);
 
             // counter
-            // currentSlideEl.innerHTML = this.activeIndex;
-            // allSlidesEl.innerHTML = `/ ${this.slides.length}`;
+            currentSlideEl.innerHTML = this.activeIndex;
+            allSlidesEl.innerHTML = `/ ${this.slides.length}`;
 
-            // if (currentSlideEl && allSlidesEl) {
-            //   if (loop) {
-            //     currentSlideEl.innerHTML = active + 1;
-            //     allSlidesEl.innerHTML = `/ ${count}`;
-            //   }
-            // }
+            if (currentSlideEl && allSlidesEl) {
+              currentSlideEl.innerHTML = this.activeIndex + 1;
+              allSlidesEl.innerHTML = `/ ${this.slides.length}`;
+            }
           }
         }
       });
@@ -232,6 +285,8 @@ document.addEventListener("DOMContentLoaded", () => {
         item.addEventListener("click", function () {
           cardThumbnailsMain.classList.add("changing");
           cardThumbnailsBottom.classList.add("changing");
+          cardThumbnails.forEach(i => i.parentElement.classList.remove("active"));
+          this.parentElement.classList.add("active");
           setTimeout(() => {
             cardThumbnailsMainImg.src = item.src;
             cardThumbnailsBottomImg.src = item.src;
@@ -273,9 +328,9 @@ document.addEventListener("DOMContentLoaded", () => {
             const mainElWrapper = th.parentElement.parentElement.querySelector(".catalog-items__img-wrapper");
             const mainEl = th.parentElement.parentElement.querySelector(".catalog-items__img");
 
-            thumbs.forEach(z => z.classList.remove('active'));
+            thumbs.forEach(z => z.classList.remove("active"));
 
-            this.classList.add('active');
+            this.classList.add("active");
             mainElWrapper.classList.add("changing");
             setTimeout(() => {
               mainEl.src = imgSrc;
@@ -289,4 +344,61 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   };
   catalogPage();
+
+  // PAYMENT-DELIVERY PAGE
+  const paymentDeliveryPage = () => {
+    const items = document.querySelectorAll("#paymentAccordion .payment-delivery__item-title");
+
+    if (items) {
+      items.forEach(item => {
+        item.addEventListener("click", function (e) {
+          item.classList.toggle("active");
+        });
+      });
+    }
+  };
+  paymentDeliveryPage();
+
+  // ORDERING PAGE
+  const orderingPage = () => {
+    const showMainCardBtn = document.getElementById("showMainCardBtn");
+    const showMainCard = document.getElementById("showMainCard");
+    const orderingForm = document.getElementById("orderingForm");
+    const inputs = document.querySelectorAll("#orderingForm [name='orderingDelivery']");
+
+    if (orderingForm && inputs) {
+      inputs.forEach(item =>
+        item.addEventListener("change", function (e) {
+          if (this.value === "Самовывоз") {
+            document.querySelector(".ordering__delivery-fields").classList.add("hidden");
+          } else {
+            document.querySelector(".ordering__delivery-fields").classList.remove("hidden");
+          }
+        })
+      );
+    }
+
+    if (showMainCardBtn && showMainCard) {
+      showMainCardBtn.addEventListener("click", function (e) {
+        this.classList.toggle("active");
+        showMainCard.classList.toggle("active");
+      });
+    }
+  };
+  orderingPage();
+
+  const cabinetOrdersPage = () => {
+    const order__header = document.querySelectorAll('.order__header');
+    if (order__header && window.innerWidth <= 991) {
+      if(order__header.length >= 0) {
+        order__header.forEach(item=>{
+          item.addEventListener('click', function(e){
+            item.parentElement.classList.toggle('order_active');
+            item.nextElementSibling.classList.toggle('active');
+          })
+        })
+      }
+    }
+  } 
+  cabinetOrdersPage();
 });
